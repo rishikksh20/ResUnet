95e985eaa8ea552e77d7e25d2bd1f28d2a84c5c2
rishikksh20 <rishikksh20@gmail.com>
2020-09-07 14:29:08 +0530

    :boom: Add GAN structure and training

--- core/res_unet_plus.py
+++ core/res_unet_plus.py
@@ -51,7 +51,7 @@ class ResUnetPlusPlus(nn.Module):
 
         self.aspp_out = ASPP(filters[1], filters[0])
 
-        self.output_layer = nn.Sequential(nn.Conv2d(filters[0], 1, 1), nn.Sigmoid())
+        self.output_layer = nn.Sequential(nn.Conv2d(filters[0], 1, 1)) # , nn.Sigmoid())
 
     def forward(self, x):
         x1 = self.input_layer(x) + self.input_skip(x)
